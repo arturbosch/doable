031cb0e74738c2469e5610e9450d76d49a2759ee
abosch <arturbosch@gmx.de>
2017-02-18 19:38:05 +0100

    Add test memory for testing phase if no persisted memory exists

--- doable-desktop/src/main/kotlin/io/gitlab/arturbosch/doable/data/TextDatabase.kt
+++ doable-desktop/src/main/kotlin/io/gitlab/arturbosch/doable/data/TextDatabase.kt
@@ -3,8 +3,11 @@ package io.gitlab.arturbosch.doable.data
 import com.google.gson.Gson
 import io.gitlab.arturbosch.doable.Memory
 import io.gitlab.arturbosch.doable.Persistence
+import io.gitlab.arturbosch.doable.Streak
+import io.gitlab.arturbosch.doable.WorkingList
 import tornadofx.Controller
 import tornadofx.find
+import java.util.Date
 
 /**
  * @author Artur Bosch
@@ -21,6 +24,10 @@ class TextDatabase(val dir: HomeFolder = find<DoableFolder>()) : Controller(), P
 
 	override fun load(memoryName: String): Memory {
 		val fromFile = dir.resolve(memoryName).toFile()
+		if (!fromFile.exists()) {
+			val memory = Memory("Test", mutableListOf(WorkingList("Test", mutableListOf())), mutableListOf(), Date(), 0, Streak(0))
+			save(memory)
+		}
 		return parser.fromJson(fromFile.readText(), Memory::class.java)
 	}
 
